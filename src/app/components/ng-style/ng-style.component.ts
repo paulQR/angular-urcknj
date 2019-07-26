9ea5af2ddf4a8b9ed14a1e2b29d56ecc47631e50
paulQR <paulqr10@gmail.com>
2019-07-26 12:21:58 +0200

    2 files modified, 1 added

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template:
+      <p>
+          ng-styel works!
+      </p>,
+  styles:[]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
